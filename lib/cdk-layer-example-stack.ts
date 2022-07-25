28273e907b4c134c9fce4696e911f0443d0053c8
Frank Liu <frankliu81@gmail.com>
2022-07-25 07:45:58 -0700

    pg works with npm

--- lib/cdk-layer-example-stack.ts
+++ lib/cdk-layer-example-stack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as Path from 'path'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkLayerExampleStack extends Stack {
@@ -12,5 +15,29 @@ export class CdkLayerExampleStack extends Stack {
     // const queue = new sqs.Queue(this, 'CdkLayerExampleQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const externalModules = [
+      'aws-sdk',
+      'aws-embedded-metrics',
+      'pg',
+    ]
+
+    const awsLayer = new lambda.LayerVersion(this, 'aws-layer', {
+      code: lambda.Code.fromAsset(Path.join(__dirname, 'layers/aws-layer')),
+    });
+
+    const frankLambda = new NodejsFunction(this, 'frank-lambda', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handler',
+      entry: Path.join(__dirname, `/../lambda/index.js`),
+      bundling: {
+        minify: false,
+        // 👇 don't bundle `aws-layer` layer
+        // layers are already available in the lambda env
+        externalModules: externalModules,
+      },
+      layers: [awsLayer],
+    });
+
   }
 }
